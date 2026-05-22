d2458131c4a260146e1c1226c5ace13d495503bc
Fnyyy <adifunny720@gmail.com>
2026-05-22 13:57:43 +0700

    test(katalon): add username input selector for Login page

---
+++ Katalon_Test_Cases/Object Repository/Login/Input_Username.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Username</name>
+   <tag></tag>
+   <elementGuidId>30b78252-b519-499b-bac4-d4bc339b1c8b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='login-username']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
