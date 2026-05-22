e69c81436b217bf791ec9a2649ea29e5c7c19298
Fnyyy <adifunny720@gmail.com>
2026-05-22 15:52:14 +0700

    test(katalon): add password input selector for Login page

---
+++ Katalon_Test_Cases/Object Repository/Login/Input_Password.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Password</name>
+   <tag></tag>
+   <elementGuidId>149e5718-de10-4e75-8464-c2a7b3ab877f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='login-password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
