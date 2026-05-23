75c5cd2a57fd4a3b49652b3518bc6c97a57c8801
Fnyyy <adifunny720@gmail.com>
2026-05-23 13:57:19 +0700

    test(katalon): add login button selector

---
+++ Katalon_Test_Cases/Object Repository/Login/Btn_Login.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_Login</name>
+   <tag></tag>
+   <elementGuidId>dc381886-e55d-456e-b924-f7a0bb53240c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='login-submit']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
