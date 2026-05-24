de73b5cc6e3de3ac8aa13921cf947fb974ac1a86
Fnyyy <adifunny720@gmail.com>
2026-05-24 16:40:03 +0700

    test(katalon): add research management section locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Text_ResearchManagement.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_ResearchManagement</name>
+   <tag></tag>
+   <elementGuidId>2663826b-956a-4d94-92bd-1e36f70fb0f7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h1[contains(text(), 'Research Management')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
