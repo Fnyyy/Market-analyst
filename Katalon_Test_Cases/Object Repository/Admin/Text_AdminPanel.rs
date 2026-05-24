fea6e99597f6c1b9e944d459442bbea8d288b104
Fnyyy <adifunny720@gmail.com>
2026-05-24 10:27:08 +0700

    test(katalon): add admin panel header locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Text_AdminPanel.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_AdminPanel</name>
+   <tag></tag>
+   <elementGuidId>29b54559-c01e-4da7-8485-90853491cdb7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(@class, 'admin-brand-name') and contains(text(), 'Sisvest')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
