2b8292bdec0d578cd9d90cb707e2cad3b8122961
Fnyyy <adifunny720@gmail.com>
2026-05-24 14:11:11 +0700

    test(katalon): add user management section locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Text_UserManagement.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_UserManagement</name>
+   <tag></tag>
+   <elementGuidId>7f69c2c4-196d-4699-9244-75f4421e9504</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h1[contains(text(), 'User Management')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
