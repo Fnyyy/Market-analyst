3264b24f0b619b5e4739d572ce79399726d63bdd
Fnyyy <adifunny720@gmail.com>
2026-05-25 15:20:32 +0700

    test(katalon): add user name budi cell locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Table/Text_UserName_Budi.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_UserName_Budi</name>
+   <tag></tag>
+   <elementGuidId>c02a8f8d-2f83-420b-820a-9ed25fdcea1e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='admin-user-name' and contains(text(), 'budi')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
