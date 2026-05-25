2cd5fd7f17345275ba8acfee14e1dfa7a5390ae1
Fnyyy <adifunny720@gmail.com>
2026-05-25 14:08:30 +0700

    test(katalon): add user search bar locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Input_SearchUser.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_SearchUser</name>
+   <tag></tag>
+   <elementGuidId>6d20405a-59bd-445f-a460-461a126f454f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@placeholder='Search users...']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
