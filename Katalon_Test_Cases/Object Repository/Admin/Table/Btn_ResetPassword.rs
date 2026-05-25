9d2be4a85d551d23ff89ddf8fb5ac19bf509eb6a
Fnyyy <adifunny720@gmail.com>
2026-05-25 16:58:38 +0700

    test(katalon): add reset password button selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Table/Btn_ResetPassword.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_ResetPassword</name>
+   <tag></tag>
+   <elementGuidId>d87b28c5-1107-49d9-8684-8b65f3ef8d7e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//tr[descendant::div[@class='admin-user-name' and contains(text(), 'budi')]]//button[contains(@class, 'admin-action-btn') and contains(text(), 'Reset PW')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
