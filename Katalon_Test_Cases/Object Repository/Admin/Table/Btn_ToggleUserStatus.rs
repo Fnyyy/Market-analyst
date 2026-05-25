df571cd0114f00249575993afe0e65a0b563e43f
Fnyyy <adifunny720@gmail.com>
2026-05-25 16:13:54 +0700

    test(katalon): add user active toggle button selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Table/Btn_ToggleUserStatus.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_ToggleUserStatus</name>
+   <tag></tag>
+   <elementGuidId>13c725ae-0ddd-436c-ba49-fe3043b8461f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//tr[descendant::div[@class='admin-user-name' and contains(text(), 'budi')]]//button[contains(@class, 'admin-action-btn') and (contains(text(), 'Deactivate') or contains(text(), 'Activate'))]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
