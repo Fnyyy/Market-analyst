0fab69d90a986f513994c82f6bc2becdbc389ffa
Fnyyy <adifunny720@gmail.com>
2026-05-26 17:21:09 +0700

    test(katalon): add create new research report button selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Btn_NewReport.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_NewReport</name>
+   <tag></tag>
+   <elementGuidId>ce5d780e-98a6-4cc6-ae77-67ccb71e1a8d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(@class, 'admin-btn') and contains(., 'New Report')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
