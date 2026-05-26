e45c01dbf82f9071eff45a522548a460999fad5d
Fnyyy <adifunny720@gmail.com>
2026-05-26 11:10:22 +0700

    test(katalon): add reset password modal title locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal/Text_ResetPasswordTitle.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_ResetPasswordTitle</name>
+   <tag></tag>
+   <elementGuidId>c8fff643-e8b6-4a4c-bda5-d60097a80eea</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h3[contains(text(), 'Reset Password')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
