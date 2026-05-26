e8a0337bbf10eea8082c0a72e435ad9c91aad431
Fnyyy <adifunny720@gmail.com>
2026-05-26 15:21:02 +0700

    test(katalon): add confirm password input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal/Input_ConfirmPassword.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_ConfirmPassword</name>
+   <tag></tag>
+   <elementGuidId>f46f590f-6242-4a7c-bb53-840ee5d17ea9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@placeholder='Repeat password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
