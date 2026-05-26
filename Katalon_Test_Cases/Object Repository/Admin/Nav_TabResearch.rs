ef08a2a29601d74c01925ccbb8f849cbe080be36
Fnyyy <adifunny720@gmail.com>
2026-05-26 16:51:37 +0700

    test(katalon): add research tab navigation selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Nav_TabResearch.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Nav_TabResearch</name>
+   <tag></tag>
+   <elementGuidId>8b803654-15a9-4105-8a20-28a8d77770da</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(@class, 'admin-nav-item') and contains(., 'Research Management')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
