6f30bf1d35d4844cd7d4c8a9c9d7d305c4e1fd39
Fnyyy <adifunny720@gmail.com>
2026-05-26 13:13:18 +0700

    test(katalon): add new password input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal/Input_NewPassword.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_NewPassword</name>
+   <tag></tag>
+   <elementGuidId>dfe6898e-45ac-43cf-ba6e-5a04e5e0059b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@placeholder='Min 6 characters']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
