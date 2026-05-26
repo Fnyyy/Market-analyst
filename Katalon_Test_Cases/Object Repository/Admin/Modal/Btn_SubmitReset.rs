267a91c2491171e9aece6b2976fcd80beede9509
Fnyyy <adifunny720@gmail.com>
2026-05-26 16:03:41 +0700

    test(katalon): add submit reset password button selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal/Btn_SubmitReset.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_SubmitReset</name>
+   <tag></tag>
+   <elementGuidId>f6c30568-bace-45b4-b391-7b31ed93622c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit' and contains(., 'Reset')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
