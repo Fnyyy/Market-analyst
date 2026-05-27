8b4fb5e5d6581ac0a492806d401b13e53f3a308e
Fnyyy <adifunny720@gmail.com>
2026-05-27 09:30:04 +0700

    test(katalon): add stock ticker input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_Ticker.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Ticker</name>
+   <tag></tag>
+   <elementGuidId>acdbb988-9753-4f8b-afcc-3bab1ea682ed</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='ticker']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
