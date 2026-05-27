89489e5588f8978816852647eee592fab0aa4428
Fnyyy <adifunny720@gmail.com>
2026-05-27 15:24:01 +0700

    test(katalon): add stock current price input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_CurrentPrice.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_CurrentPrice</name>
+   <tag></tag>
+   <elementGuidId>79fb32ea-d269-49b3-b042-eb23082110e0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='currentPrice']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
