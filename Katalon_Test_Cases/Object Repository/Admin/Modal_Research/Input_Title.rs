3c6b981b3db18665f549a3db42ebe785d99e0047
Fnyyy <adifunny720@gmail.com>
2026-05-27 10:01:54 +0700

    test(katalon): add research title input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_Title.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Title</name>
+   <tag></tag>
+   <elementGuidId>14e5c011-da04-4ff9-9119-25ae2fac391d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='title']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
