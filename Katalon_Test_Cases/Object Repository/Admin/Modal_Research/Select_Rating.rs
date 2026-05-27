74e37a448b3fba7837c4ebc385514600a766e442
Fnyyy <adifunny720@gmail.com>
2026-05-27 12:08:30 +0700

    test(katalon): add rating dropdown selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Select_Rating.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Select_Rating</name>
+   <tag></tag>
+   <elementGuidId>b191f702-db63-400c-a928-40684549e52b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name='rating']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
