feb0638d4b2ca90c324d51cdbd1931a8e5f18250
Fnyyy <adifunny720@gmail.com>
2026-05-28 09:20:38 +0700

    test(katalon): add stock target price input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_TargetPrice.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_TargetPrice</name>
+   <tag></tag>
+   <elementGuidId>8641eeba-f15c-49e1-8340-2084eb7f7e86</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='targetPrice']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
