3de49c9d2130a88d572f34901f6318ca45c02d83
Fnyyy <adifunny720@gmail.com>
2026-05-28 11:21:04 +0700

    test(katalon): add tag items input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_Tags.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Tags</name>
+   <tag></tag>
+   <elementGuidId>105f93f6-0d0b-431c-bf0d-3c8ee434870b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='tags']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
