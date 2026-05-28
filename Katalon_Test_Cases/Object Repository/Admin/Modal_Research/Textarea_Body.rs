49693228d260e121b0439a9e4f152e8091e19841
Fnyyy <adifunny720@gmail.com>
2026-05-28 13:02:19 +0700

    test(katalon): add report content body textarea locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Textarea_Body.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Textarea_Body</name>
+   <tag></tag>
+   <elementGuidId>cd6cbce4-c370-408f-9867-a396836e037c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//textarea[@name='body']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
