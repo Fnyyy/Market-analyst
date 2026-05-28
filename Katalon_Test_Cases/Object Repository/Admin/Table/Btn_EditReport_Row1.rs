df35c7e6d879ff3ee55d961e8b2ec5dd2d36001b
Fnyyy <adifunny720@gmail.com>
2026-05-28 16:25:14 +0700

    test(katalon): add edit report action button locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Table/Btn_EditReport_Row1.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_EditReport_Row1</name>
+   <tag></tag>
+   <elementGuidId>271fca59-dfec-4b8c-9c49-ff528e25f6f5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[contains(@class, 'admin-action-btn') and contains(., 'Edit')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
