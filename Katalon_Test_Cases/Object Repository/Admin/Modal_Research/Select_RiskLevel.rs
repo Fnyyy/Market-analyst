b4476c68b474a5cdd59e8d4928b939aed21d1934
Fnyyy <adifunny720@gmail.com>
2026-05-28 10:04:01 +0700

    test(katalon): add risk level dropdown selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Select_RiskLevel.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Select_RiskLevel</name>
+   <tag></tag>
+   <elementGuidId>b1fa032f-b04a-4138-b1f4-02b97e0bf160</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name='riskLevel']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
