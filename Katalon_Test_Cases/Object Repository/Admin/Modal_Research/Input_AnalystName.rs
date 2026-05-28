7479c85c6cac1cfa5a22a6c9424db08b60fe224a
Fnyyy <adifunny720@gmail.com>
2026-05-28 10:34:57 +0700

    test(katalon): add analyst name input locator

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Input_AnalystName.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_AnalystName</name>
+   <tag></tag>
+   <elementGuidId>b940170f-8e70-4657-838c-01b0c46b1441</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='analystName']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
