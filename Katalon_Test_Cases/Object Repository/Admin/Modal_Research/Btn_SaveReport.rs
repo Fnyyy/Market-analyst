4d6343049cdaac77843c09a5ca55aa2bfccf5147
Fnyyy <adifunny720@gmail.com>
2026-05-28 16:08:22 +0700

    test(katalon): add save report button selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Modal_Research/Btn_SaveReport.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_SaveReport</name>
+   <tag></tag>
+   <elementGuidId>d8fe7096-6457-42ff-bee4-d260a9757ace</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@type='submit' and contains(., 'Save Report')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
