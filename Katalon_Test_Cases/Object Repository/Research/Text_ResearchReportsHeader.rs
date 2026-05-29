eb121d2ac8f1170102e47c62777954730edde17c
Fnyyy <adifunny720@gmail.com>
2026-05-29 17:26:29 +0700

    test(katalon): add research page header locator

---
+++ Katalon_Test_Cases/Object Repository/Research/Text_ResearchReportsHeader.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_ResearchReportsHeader</name>
+   <tag></tag>
+   <elementGuidId>4645bbd9-8bee-42fa-b003-cd6b971c68ef</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h1[contains(text(), 'Research Reports')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
