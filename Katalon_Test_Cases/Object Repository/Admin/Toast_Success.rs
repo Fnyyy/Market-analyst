78b43949f26715bc373ff0437ce47ea7edc3a699
Fnyyy <adifunny720@gmail.com>
2026-05-29 13:59:06 +0700

    test(katalon): add success notification toast selector

---
+++ Katalon_Test_Cases/Object Repository/Admin/Toast_Success.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Toast_Success</name>
+   <tag></tag>
+   <elementGuidId>00faafc9-0ab4-4769-8ed7-768ca1e1d321</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class, 'admin-toast') and contains(@class, 'admin-toast--success')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
