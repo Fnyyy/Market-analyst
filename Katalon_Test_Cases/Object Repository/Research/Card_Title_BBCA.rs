b2ee135cc58512c68ff83ef2a7f50082a4303ad4
Fnyyy <adifunny720@gmail.com>
2026-05-31 09:05:27 +0700

    test(katalon): add BBCA stock card locator

---
+++ Katalon_Test_Cases/Object Repository/Research/Card_Title_BBCA.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Card_Title_BBCA</name>
+   <tag></tag>
+   <elementGuidId>9408587d-82e0-4d6a-9568-48dc3c4ea04b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
