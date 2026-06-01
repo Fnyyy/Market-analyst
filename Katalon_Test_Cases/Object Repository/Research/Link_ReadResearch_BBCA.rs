2a66efef990f576acb38a8afa5b7881eed3ad30b
Fnyyy <adifunny720@gmail.com>
2026-06-01 09:02:04 +0700

    test(katalon): add read research link locator

---
+++ Katalon_Test_Cases/Object Repository/Research/Link_ReadResearch_BBCA.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Link_ReadResearch_BBCA</name>
+   <tag></tag>
+   <elementGuidId>8e6bdfd1-dd94-4a0a-adfb-2c45e8f03795</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(@class, 'read-more')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
