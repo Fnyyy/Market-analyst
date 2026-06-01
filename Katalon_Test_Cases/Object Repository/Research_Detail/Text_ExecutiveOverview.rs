d9a4f70b9cf2e5518aeecb1cbabad97da859710a
Fnyyy <adifunny720@gmail.com>
2026-06-01 10:32:14 +0700

    test(katalon): add executive overview container locator

---
+++ Katalon_Test_Cases/Object Repository/Research_Detail/Text_ExecutiveOverview.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Text_ExecutiveOverview</name>
+   <tag></tag>
+   <elementGuidId>8868275b-a711-4583-b684-992c3553c776</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h2[contains(text(), 'Executive Overview')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
